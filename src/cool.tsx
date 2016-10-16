7520a7efba4c66c2a03b574fe9153305e1158589
pdl5p <paul.lucas@fivep.com.au>
2016-10-16 17:22:01 +1100

    Init

---
+++ src/cool.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react';
+
+export default class CoolComponent extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+      <h2>TSX!</h2>
+      </div>
+    );
+    //return React.createElement("div", null, "Testings BANG");
+  }
+}
